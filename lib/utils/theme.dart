7440b5978f8f1c2313681fcee9bed8a0c225e5a5
BrayanMota <brayanmota2001@hotmail.com>
2023-04-21 18:53:59 -0300

    ✨ Criando um arquivo contendo as configurações do tema geral do aplicativo

---
+++ lib/utils/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTheme {
+  ThemeData themeData() {
+    return ThemeData(
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}

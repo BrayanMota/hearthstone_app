46e49bdff356e2251abbb44b256a87f289d2a9ef
BrayanMota <brayanmota2001@hotmail.com>
2023-04-21 19:08:16 -0300

    ✨ Criando a tela principal do aplicativo com um banner e lista dos heróis

---
+++ lib/pages/home.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:hearthstone_app/customs/custom_label.dart';
+import 'package:hearthstone_app/utils/mock.dart';
+
+import '../utils/colors.dart';
+import '../utils/dimensions.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.corPrincipal,
+      // backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          'Bem Vindo, Samir',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        actions: const [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/images/avatar_example.png'),
+          )
+        ],
+      ),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.symmetric(
+        horizontal: CustomDimensions.overallScreenBodyDistance,
+      ),
+      children: [
+        _buildCardCreateDeck(),
+        _buildListHeroes(),
+      ],
+    );
+  }
+
+  Widget _buildCardCreateDeck() {
+    return Container(
+      // height: MediaQuery.of(context).size.height * 0.30,
+      height: 220,
+      decoration: BoxDecoration(
+        image: const DecorationImage(
+          image: AssetImage('assets/images/create_deck.png'),
+          fit: BoxFit.fill,
+        ),
+        borderRadius: BorderRadius.circular(24),
+      ),
+    );
+  }
+
+  Widget _buildListHeroes() {
+    return Column(
+      children: [
+        const CustomLabel(
+          title: 'Heróis',
+          seeAll: false,
+        ),
+        Container(
+          height: 100,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: listHeroes.length,
+            itemBuilder: ((context, index) {
+              return Column(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    margin: EdgeInsets.only(
+                      right: 10.0,
+                    ),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(
+                          '${listHeroes[index]['icon_hero']}',
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Text(
+                  //   '${listHeroes[index]['name_hero']}',
+                  //   style: const TextStyle(
+                  //     color: Colors.white,
+                  //   ),
+                  // )
+                ],
+              );
+            }),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/utils/mock.dart
@@ -0,0 +1,57 @@
+final listHeroes = [
+  {
+    'name_hero': 'Cavaleiro da Morte',
+    'image_hero': 'assets/images/heroes/cavaleiro_morte.png',
+    'icon_hero': 'assets/images/heroes/icons/cavaleiro_morte.png',
+  },
+  {
+    'name_hero': 'Caçador de Demônios',
+    'image_hero': 'assets/images/heroes/cacador_demonios.png',
+    'icon_hero': 'assets/images/heroes/icons/cacador_demonios.png',
+  },
+  {
+    'name_hero': 'Druida',
+    'image_hero': 'assets/images/heroes/druida.png',
+    'icon_hero': 'assets/images/heroes/icons/druida.png',
+  },
+  {
+    'name_hero': 'Caçador',
+    'image_hero': 'assets/images/heroes/cacador.png',
+    'icon_hero': 'assets/images/heroes/icons/cacador.png',
+  },
+  {
+    'name_hero': 'Mago',
+    'image_hero': 'assets/images/heroes/mago.png',
+    'icon_hero': 'assets/images/heroes/icons/mago.png',
+  },
+  {
+    'name_hero': 'Paladino',
+    'image_hero': 'assets/images/heroes/paladino.png',
+    'icon_hero': 'assets/images/heroes/icons/paladino.png',
+  },
+  {
+    'name_hero': 'Sacerdote',
+    'image_hero': 'assets/images/heroes/sacerdote.png',
+    'icon_hero': 'assets/images/heroes/icons/sacerdote.png',
+  },
+  {
+    'name_hero': 'Ladino',
+    'image_hero': 'assets/images/heroes/ladino.png',
+    'icon_hero': 'assets/images/heroes/icons/ladino.png',
+  },
+  {
+    'name_hero': 'Xamã',
+    'image_hero': 'assets/images/heroes/xama.png',
+    'icon_hero': 'assets/images/heroes/icons/xama.png',
+  },
+  {
+    'name_hero': 'Bruxo',
+    'image_hero': 'assets/images/heroes/bruxo.png',
+    'icon_hero': 'assets/images/heroes/icons/bruxo.png',
+  },
+  {
+    'name_hero': 'Guerreiro',
+    'image_hero': 'assets/images/heroes/guerreiro.png',
+    'icon_hero': 'assets/images/heroes/icons/guerreiro.png',
+  },
+];
